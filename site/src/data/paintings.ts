9af48534c705c988d26aa541b4420aa8589ce400
Diego Arnoldo Azuela Rosas <diego.azuela@gmail.com>
2026-09-06 19:00:18 -0700

    syntax correction:

--- site/src/data/paintings.ts
+++ site/src/data/paintings.ts
@@ -6,7 +6,7 @@ export interface Painting {
 }
 
 export const paintings: Painting[] = [
-  { title: 'My Muse, Z', image: '/media/paintings/MyMuse_Z.jpg' },
+  { title: 'Dancing Flowers', image: '/media/paintings/DancingFlowers.jpg' },
   { title: 'Heavy Is the Head', image: '/media/paintings/HeavyIsTheHead.jpg' },
   { title: 'Music’s Picasso — Father’s Gift', image: '/media/paintings/MusicsPicasso_FathersGift.jpg' },
 ];
